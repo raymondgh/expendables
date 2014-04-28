c2748f4cbae3a42b93186d29b2be6c1f64bd4e88
Chris Smith <ChrisMBP@mbp.att.net>
2014-04-27 17:59:52 -0700

    cleaned folder

--- parseapp/cloud/app.js
+++ parseapp/cloud/app.js
@@ -1,78 +1,80 @@
+// These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
-var http = require('http');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var nconf = require('nconf');
-
-nconf.env().argv();
-nconf.file('./config.json');
-
-nconf.set('AUTOMATIC_SCOPES', 'scope:trip:summary scope:location scope:vehicle scope:notification:hard_accel scope:notification:hard_brake scope:notification:speeding');
-
-
-var routes = require('./routes');
-var oauth = require('./routes/oauth');
-var trips = require('./routes/trips');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+ 
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+ 
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+ 
+app.post('/webhook', function(req, res) {
+  //res.render('hello', { message: 'Congrats, you just set up your app!' });
+  console.log(req.body);
+  res.end('{"success" : "Updated Successfully"}');
+  //res.render('hello', { message: req.body.id });
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser('rXrq6xCSJu'));
-app.use(express.session());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
-
-if (app.get('env') !== 'development') {
-	app.all('*', routes.force_https);
-}
-
-app.get('/', routes.index);
-
-app.get('/authorize/', oauth.authorize);
-app.get('/logout/', oauth.logout);
-app.get('/redirect/', oauth.redirect);
-
-app.get('/api/trips/', oauth.authenticate, trips.trips);
-app.get('/download/trips.json', oauth.authenticate, trips.downloadTripsJSON);
-app.get('/download/trips.csv', oauth.authenticate, trips.downloadTripsCSV);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+});
+ 
+app.get('/redirect', function(req, res) {
+  //res.render('hello', { message: 'Congrats, you just set up your app!' });
+  console.log(req.body);
+  //res.render('hello', { message: req });
 });
 
-/// error handlers
+/*app.get('/simulate', function(req, res) {
+  //res.render('hello', { message: 'Congrats, you just set up your app!' });
+  //console.log(req);
+  //res.render('hello', { message: req });
+  
+var TripObject = Parse.Object.extend("Trips");
+var tripObject = new TripObject();
+var tripID = null;
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
+tripObject.set(
+  {
+        "average_mpg": 21,
+        "business": true,
+        "distance_m": 4777.44444444467,
+        "end_location": "Test Loca",
+        "end_time": 1398041460124,
+        "fuel_cost_usd": 2.344,
+        "start_location": "Start Location",
+        "start_time": 1398038541600
+    }
+  );
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+
+ 
+tripObject.save(null, {
+  success: function(tripObject) {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + tripObject.id);
+    console.log(tripObject.id.toString());
+
+
+  },
+  error: function(tripObject, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and description.
+   
+  }
 });
-
-module.exports = app;
+}*/
+// // Example reading from the request query string of an HTTP get request.
+// app.get('/test', function(req, res) {
+//   // GET http://example.parseapp.com/test?message=hello
+//   res.send(req.query.message);
+// });
+ 
+// // Example reading from the request body of an HTTP post request.
+// app.post('/test', function(req, res) {
+//   // POST http://example.parseapp.com/test (with request body "message=hello")
+//   res.send(req.body.message);
+// });
+ 
+// Attach the Express app to Cloud Code.
+app.listen();
