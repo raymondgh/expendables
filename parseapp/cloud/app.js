4788f1e872c7a3c770212e3f749fe472b1995970
Harvey <harvey@harveychan.net>
2014-04-27 19:00:42 -0700

    added parse push on webhook trigger

--- parseapp/cloud/app.js
+++ parseapp/cloud/app.js
@@ -13,11 +13,53 @@ app.use(express.bodyParser());    // Middleware for reading request body
  
 app.post('/webhook', function(req, res) {
   //res.render('hello', { message: 'Congrats, you just set up your app!' });
-  console.log(req.body);
-  res.end('{"success" : "Updated Successfully"}');
+    console.log(req.body);
+    SaveTrip(req, res)
+  //res.end('{"success" : "Updated Successfully"}');
   //res.render('hello', { message: req.body.id });
 
 });
+
+function SaveTrip(req,res){
+	  var Trip = Parse.Object.extend('Trip');
+	  var tripQuery = new Parse.Query(Trip);
+	  var foundTrip;
+	  console.log("searching for existing trip object");
+	  Parse.Push.send({
+	      channels: ["harvey"],
+	      data: {
+	          "action":"com.parseapp.expensetracker.toq.UPDATE_STATUS",
+	          "tripId":"1"
+	      }
+	  }, {
+	      success: function () {
+	          // Push was successful
+	      },
+	      error: function (error) {
+	          // Handle error
+	      }
+	  });
+
+	  tripQuery.get(req.params.objectId).then(function(trip) {
+	      if (trip) {
+	          foundtrip = trip;
+	          foundtrip.set("business", req.body.business);
+	          console.log("about to save existing trip object");
+	          foundtrip.save().then(function (result) {
+	              res.end('{"success" : "Updated Successfully"}');
+	          });
+	      } else {
+	          var newTrip = new Trip();
+	          newTrip.set("business", "false");
+	          console.log("about to save new trip object");
+	          newTrip.save().then(function (result) {
+	              res.end('{"success" : "new object created"}');
+	          });
+	          
+	      }
+	});
+}
+	
  
 app.get('/redirect', function(req, res) {
   //res.render('hello', { message: 'Congrats, you just set up your app!' });
